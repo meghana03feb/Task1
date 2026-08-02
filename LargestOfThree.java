16e485f0e71976948c80e2317b941fb75cbb3a84
meghana03feb <andavarapumeghana.25.csm@anits.edu.in>
2026-08-02 09:49:15 +0530

    Add LargestOfThree program to find maximum number

---
+++ LargestOfThree.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class LargestOfThree {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a, b, c;
+
+        System.out.print("Enter first number: ");
+        a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        b = sc.nextInt();
+
+        System.out.print("Enter third number: ");
+        c = sc.nextInt();
+
+        if (a >= b && a >= c) {
+            System.out.println("Largest number = " + a);
+        } else if (b >= a && b >= c) {
+            System.out.println("Largest number = " + b);
+        } else {
+            System.out.println("Largest number = " + c);
+        }
+
+        sc.close();
+    }
+}
